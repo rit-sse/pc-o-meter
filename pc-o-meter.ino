bb3f336c8f32ee6e7eaf924e51db1a32b7184b85
Corban Mailloux <github@corb.co>
2015-10-07 21:54:23 -0400

    Initial version using a Particle Photon. No mentoring mode.

---
+++ pc-o-meter.ino
@@ -0,0 +1,59 @@
+// Servo configuration
+int servoPin = A4;
+int servoMin = 0;
+int servoMax = 160;
+int wtfAngle = 20; // Angle to increase by for each button press
+
+// Decay configuration
+int servoDecayDelay = 2000; // Delay after button press before decay
+int servoDecaySpeed = 250; // Decay rate; smaller = faster decay
+
+
+unsigned long lastPressTime;
+Servo servo;
+int currentAngle = 0;
+
+void setup() {
+    servo.attach(servoPin);
+    Particle.function("wtf", trigger);
+    RGB.control(true);
+}
+
+void loop() {
+    // Update light color
+    if (currentAngle >= (0.8 * servoMax)) {
+        RGB.color(255, 0, 0);
+    } else if (currentAngle >= (0.6 * servoMax)) {
+        RGB.color(255, 128, 0);
+    } else if (currentAngle >= (0.4 * servoMax)) {
+        RGB.color(255, 255, 0);
+    } else if (currentAngle >= (0.2 * servoMax)) {
+        RGB.color(0, 255, 0);
+    } else {
+        RGB.color(0, 0, 255);
+    }
+
+    if ((millis() - lastPressTime) >= servoDecayDelay) {
+        if (currentAngle > servoMin) {
+            updateServo(currentAngle - 1);
+            delay(servoDecaySpeed);
+        }
+    }
+}
+
+void updateServo(int angle) {
+    if (angle < servoMin) {
+        angle = servoMin;
+    } else if (angle > servoMax) {
+        angle = servoMax;
+    }
+
+    currentAngle = angle;
+    servo.write(currentAngle);
+}
+
+int trigger(String UNUSED) {
+    updateServo(currentAngle + wtfAngle);
+    lastPressTime = millis();
+    return(currentAngle);
+}
